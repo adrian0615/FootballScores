c862f99be9a7f6b602667c8eabf0a2584a03983e
Adrian McDaniel <adriandmcdaniel@gmail.com>
2016-12-08 13:38:38 -0500

    Initial Commit

---
+++ FootballScores/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FootballScores
+//
+//  Created by Adrian McDaniel on 12/8/16.
+//  Copyright © 2016 dssafsfsd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
